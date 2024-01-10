802eed96f681a603ea79f7a69dd6526491152abe
ibaddKhan <ibadurrehman718@gmail.com>
2024-01-10 05:33:57 +0500

    added navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,17 +6,25 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-
+import { Link } from 'react-router-dom';
 export default function ButtonAppBar() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+    <Box  sx={{ flexGrow: 1 }}>
+      <AppBar position="absolute">
         <Toolbar>
          
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             News
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Link to='/'>
+          <Button color="inherit">Home</Button>
+          </Link>
+          <Link to='products'>
+          <Button color="inherit">Products</Button>
+          </Link>
+          <Link to='About'>
+          <Button color="inherit">About</Button>
+          </Link>
         </Toolbar>
       </AppBar>
     </Box>
